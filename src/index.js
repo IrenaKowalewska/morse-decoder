2c2c9cdeec392985ef472cbeb9f5893e6de677f0
kleo4004 <zkioz4004@gmail.com>
2020-03-19 21:29:36 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,15 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodeExpr = '',
+    lengthExpr = expr.length;
+    
+
+for (let i=0; i < lengthExpr; i += 10){
+    let letter = expr.slice(i, i +10).replace(/\*/g, '').replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-');
+    decodeExpr += MORSE_TABLE[letter] || ' ';   
+}
+return decodeExpr;
 }
 
 module.exports = {
